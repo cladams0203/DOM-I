d6afc5b54c7d1d145245005d993179353ef3e4a9
cladams <cladams0203@gmail.com>
2019-08-06 14:43:26 -0400

    practice

--- js/index.js
+++ js/index.js
@@ -40,3 +40,9 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const aTags = document.querySelectorAll('nav a');
+console.log(aTags);
+aTags.forEach((e) => {
+  e.appendChild(siteContent.nav["nav-item-1"]);
+})
